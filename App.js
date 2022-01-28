9e92f2daafe2fe1d03280e8590eab1616d9695df
Mickael Lecoq <mickael@lecoq.me>
2022-01-28 15:58:06 +0100

    example on marker issue

--- App.js
+++ App.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Svg, Rect } from "react-native-svg";
+import { Svg, Line, Defs, Marker, Polyline } from "react-native-svg";
 import Animated, { useAnimatedProps } from "react-native-reanimated";
 
-const AnimatedRectangle = Animated.createAnimatedComponent(Rect);
+const AnimatedLine = Animated.createAnimatedComponent(Line);
 
 export default function App() {
-  const rectangleAnimatedProps = useAnimatedProps(() => {
+  const lineAnimatedProps = useAnimatedProps(() => {
     return {
-      x: 10,
-      y: 10,
-      width: 50,
-      height: 50,
-      stroke: "rgb(0, 0, 0)",
-      fill: "none",
-      strokeWidth: 1,
+      x1: 50,
+      y1: 50,
+      x2: 200,
+      y2: 200,
+      stroke: "rgb(0,0,0)",
+      strokeWidth: 10,
+      markerEnd: "url(#arrowhead)",
     };
   }, []);
 
   return (
     <View style={styles.container}>
       <Svg style={{ flex: 1 }}>
-        <AnimatedRectangle animatedProps={rectangleAnimatedProps} />
+        <Defs>
+          <Marker
+            id="arrowhead"
+            markerUnits={"strokeWidth"}
+            refX="0"
+            refY="0"
+            orient="auto"
+          >
+            <Polyline
+              points="-2,-2 0,0 -2,2 0,0"
+              stroke="context-stroke"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </Marker>
+        </Defs>
+        <AnimatedLine animatedProps={lineAnimatedProps} />
       </Svg>
     </View>
   );
